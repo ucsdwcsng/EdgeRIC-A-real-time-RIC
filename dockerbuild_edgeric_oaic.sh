ec2603aa6d118caedce9a69dedad721df729adef
ushasigh <ushasighosh1@gmail.com>
2024-08-06 12:15:45 -0700

    update with docker pull image of environment

--- dockerbuild_edgeric_oaic.sh
+++ dockerbuild_edgeric_oaic.sh
@@ -7,5 +7,7 @@ systemctl stop firewalld
 sysctl -w net.ipv4.ip_forward=1
 
 #docker build --no-cache -t edgeric_base_oaic -f Dockerfile_EdgeRIC_build_oaic .
-docker build -t edgeric_base_oaic -f Dockerfile_EdgeRIC_build_oaic .
+# docker build -t edgeric_base_oaic -f Dockerfile_EdgeRIC_build_oaic .
+
+docker build -t edgeric_base_oaic_environment -f Dockerfile_EdgeRIC_build_oaic .
 
--- dockerrun_edgeric_oaic.sh
+++ dockerrun_edgeric_oaic.sh
@@ -10,10 +10,12 @@ systemctl stop firewalld
 sysctl -w net.ipv4.ip_forward=1
 
 
-docker build -t edgeric_base_files_oaic -f Dockerfile_EdgeRIC_run_oaic .
+# docker build -t edgeric_base_files_oaic -f Dockerfile_EdgeRIC_run_oaic .
 
-docker run -it --rm --network=$1 --name edgeric_$2 --privileged=true -e DISPLAY=$DISPLAY --env=NVIDIA_DRIVER_CAPABILITiES=all --env=NVIDIA-VISIBLE_DEVICES=all --env=QT_X11_NO_MITSHM=1 -v $(pwd):/home/EdgeRIC-A-real-time-RIC:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev:/dev edgeric_base_files_oaic bash
+# docker run -it --rm --network=$1 --name edgeric_$2 --privileged=true -e DISPLAY=$DISPLAY --env=NVIDIA_DRIVER_CAPABILITiES=all --env=NVIDIA-VISIBLE_DEVICES=all --env=QT_X11_NO_MITSHM=1 -v $(pwd):/home/EdgeRIC-A-real-time-RIC:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev:/dev edgeric_base_files_oaic bash
  
+# docker run -it --rm --network=$1 --name edgeric_$2 --privileged=true -e DISPLAY=$DISPLAY --env=NVIDIA_DRIVER_CAPABILITiES=all --env=NVIDIA-VISIBLE_DEVICES=all --env=QT_X11_NO_MITSHM=1 -v $(pwd):/home/EdgeRIC-A-real-time-RIC:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev:/dev nlpurnhyun/edgeric_base_files_oaic bash
 
+docker run -it --rm --network=$1 --name edgeric_$2 --privileged=true -e DISPLAY=$DISPLAY --env=NVIDIA_DRIVER_CAPABILITiES=all --env=NVIDIA-VISIBLE_DEVICES=all --env=QT_X11_NO_MITSHM=1 -v $(pwd):/home/EdgeRIC-A-real-time-RIC:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v /dev:/dev nlpurnhyun/edgeric_base_oaic bash
 
 
